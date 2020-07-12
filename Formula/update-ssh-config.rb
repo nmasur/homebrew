ee42c2f8746035c54b4d7a632228ad76c5abc047
GitHub Action <action@github.com>
2020-07-12 17:15:56 +0000

    Version 0.1.0

--- Formula/update-ssh-config.rb
+++ Formula/update-ssh-config.rb
@@ -2,8 +2,8 @@ class UpdateSSHConfig < Formula
   desc "Modify hostname in ~/.ssh/config file"
   homepage "https://github.com/nmasur/update-ssh-config"
   url "https://github.com/nmasur/update-ssh-config/releases/latest/download/update-ssh-config-x86_64-apple-darwin.tar.gz"
-  sha256 "heythere"
-  version "0.0.1"
+  sha256 "bb45da5f8886bf8b3505bbd04358a82f15c50d8d3619fbb25ec399067b271e27"
+  version "0.1.0"
 
   def install
     bin.install "update-ssh-config"

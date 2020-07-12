316199d80c0baf00aac76b5517a9b18887479312
Noah Masur <noah.masur@take2games.com>
2020-07-12 15:36:26 -0600

    Class name title case was wrong

--- Formula/update-ssh-config.rb
+++ Formula/update-ssh-config.rb
@@ -1,4 +1,4 @@
-class UpdateSSHConfig < Formula
+class UpdateSshConfig < Formula
   desc "Modify hostname in ~/.ssh/config file"
   homepage "https://github.com/nmasur/update-ssh-config"
   url "https://github.com/nmasur/update-ssh-config/releases/latest/download/update-ssh-config-x86_64-apple-darwin.tar.gz"

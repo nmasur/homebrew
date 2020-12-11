4684fb06b57e30131608503ee80bd2e1c867d1b0
GitHub Action <action@github.com>
2020-12-11 16:27:08 +0000

    Version 0.1.3

--- Formula/bee.rb
+++ Formula/bee.rb
@@ -2,8 +2,8 @@ class Bee < Formula
   desc "Cheat the NYTimes Spelling Bee"
   homepage "https://github.com/nmasur/bee"
   url "https://github.com/nmasur/bee/releases/latest/download/bee-x86_64-apple-darwin.tar.gz"
-  sha256 "f403e7fa9624cf4ad7a07f3f4c6053c29a9b9a3402c2bdc658c855af914e1030"
-  version "0.1.2"
+  sha256 "8cfc801abadd0240774c9ccffee26429cce20cffaba86faa0bf2cc8dde0010c6"
+  version "0.1.3"
 
   def install
     bin.install "bee"
